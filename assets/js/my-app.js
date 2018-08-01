029a1e088b712dde8fc5968d6001d39de7c4f685
Andy Williams <a.williams1969@gmail.com>
2018-08-01 13:59:17 +0100

    Initial commit

---
+++ assets/js/my-app.js
@@ -0,0 +1,300 @@
+/* All the targets */
+
+targets = [
+  {
+    "Manufacturer": "Rinehart",
+    "Type": "Bear",
+    "Name": "Cinnamon Bear",
+    "Image": "Bear-CinnamonBear.png"
+  },
+  {
+    "Manufacturer": "Rinehart",
+    "Type": "Bear",
+    "Name": "Large Black Bear",
+    "Image": "Bear-LargeBlackBear.png"
+  },
+  {
+    "Manufacturer": "Rinehart",
+    "Type": "Bear",
+    "Name": "Mountain Grizzly",
+    "Image": "Bear-MountainGrizzlyBear.png"
+  },
+  {
+    "Manufacturer": "Rinehart",
+    "Type": "Bear",
+    "Name": "Polar Bear",
+    "Image": "Bear-PolarBear.png"
+  },
+  {
+    "Manufacturer": "Rinehart",
+    "Type": "Big Game",
+    "Name": "Moose",
+    "Image": "BigGame-Moose.png"
+  },
+  {
+    "Manufacturer": "Rinehart",
+    "Type": "Big Game",
+    "Name": "Pronghorn Antelope",
+    "Image": "BigGame-PronghornAntelope.png"
+  },
+  {
+    "Manufacturer": "Rinehart",
+    "Type": "Boar",
+    "Name": "Javelina",
+    "Image": "Boar-Javelina.png"
+  },
+  {
+    "Manufacturer": "Rinehart",
+    "Type": "Boar",
+    "Name": "Razor Back",
+    "Image": "Boar-RazorBack.png"
+  },
+  {
+    "Manufacturer": "Rinehart",
+    "Type": "Boar",
+    "Name": "Rising Boar",
+    "Image": "Boar-RisingBoar.png"
+  },
+  {
+    "Manufacturer": "Rinehart",
+    "Type": "Deer",
+    "Name": "30 Point Buck",
+    "Image": "Deer-30PointBuck.png"
+  },
+  {
+    "Manufacturer": "Rinehart",
+    "Type": "Deer",
+    "Name": "Alert Deer",
+    "Image": "Deer-AlertDeer.png"
+  },
+  {
+    "Manufacturer": "Rinehart",
+    "Type": "Deer",
+    "Name": "Apple Browsing Buck",
+    "Image": "Deer-AppleBrowsingBuck.png"
+  },
+  {
+    "Manufacturer": "Rinehart",
+    "Type": "Deer",
+    "Name": "Bedded Doe",
+    "Image": "Deer-BeddedDoe.png"
+  },
+  {
+    "Manufacturer": "Rinehart",
+    "Type": "Deer",
+    "Name": "Big Ten Buck",
+    "Image": "Deer-BigTenBuck.png"
+  },
+  {
+    "Manufacturer": "Rinehart",
+    "Type": "Deer",
+    "Name": "Fallow Deer",
+    "Image": "Deer-FallowDeer.png"
+  },
+  {
+    "Manufacturer": "Rinehart",
+    "Type": "Deer",
+    "Name": "Feeding Deer",
+    "Image": "Deer-FeedingDoe.png"
+  },
+  {
+    "Manufacturer": "Rinehart",
+    "Type": "Deer",
+    "Name": "Fighting Rubbing Deer",
+    "Image": "Deer-FightingRubbingBuck.png"
+  },
+  {
+    "Manufacturer": "Rinehart",
+    "Type": "Deer",
+    "Name": "Giant Mule Deer",
+    "Image": "Deer-GiantMuleDeer.png"
+  },
+  {
+    "Manufacturer": "Rinehart",
+    "Type": "Deer",
+    "Name": "Spike Buck",
+    "Image": "Deer-SpikeBuck.png"
+  },
+  {
+    "Manufacturer": "Rinehart",
+    "Type": "Exotic",
+    "Name": "Baboon",
+    "Image": "Exotic-Baboon.png"
+  },
+  {
+    "Manufacturer": "Rinehart",
+    "Type": "Exotic",
+    "Name": "Green Cobra",
+    "Image": "Exotic-GreenCobra.png"
+  },
+  {
+    "Manufacturer": "Rinehart",
+    "Type": "Exotic",
+    "Name": "Green Frog",
+    "Image": "Exotic-GreenFrog.png"
+  },
+  {
+    "Manufacturer": "Rinehart",
+    "Type": "Exotic",
+    "Name": "Hadrosaur",
+    "Image": "Exotic-Hadrosaur.png"
+  },
+  {
+    "Manufacturer": "Rinehart",
+    "Type": "Exotic",
+    "Name": "Stegosaurus",
+    "Image": "Exotic-Stegosaurus.png"
+  },
+  {
+    "Manufacturer": "Rinehart",
+    "Type": "Exotic",
+    "Name": "Velociraptor",
+    "Image": "Exotic-Velociraptor.png"
+  },
+  {
+    "Manufacturer": "Rinehart",
+    "Type": "Fish",
+    "Name": "Carp",
+    "Image": "Fish-Carp.png"
+  },
+  {
+    "Manufacturer": "Rinehart",
+    "Type": "Predator",
+    "Name": "Black Panther",
+    "Image": "Predator-BlackPanther.png"
+  },
+  {
+    "Manufacturer": "Rinehart",
+    "Type": "Predator",
+    "Name": "Coyote",
+    "Image": "Predator-Coyote.png"
+  },
+  {
+    "Manufacturer": "Rinehart",
+    "Type": "Predator",
+    "Name": "Snarling Wolf",
+    "Image": "Predator-SnarlingWolf.png"
+  },
+  {
+    "Manufacturer": "Rinehart",
+    "Type": "Predator",
+    "Name": "Wolverine",
+    "Image": "Predator-Wolverine.png"
+  },
+  {
+    "Manufacturer": "Rinehart",
+    "Type": "Sheep",
+    "Name": "Catalina Goat",
+    "Image": "Sheep-CatalinaGoat.png"
+  },
+  {
+    "Manufacturer": "Rinehart",
+    "Type": "Sheep",
+    "Name": "Stone Sheep",
+    "Image": "Sheep-StoneSheep.png"
+  },
+  {
+    "Manufacturer": "Rinehart",
+    "Type": "Turkey",
+    "Name": "Gobbler",
+    "Image": "Turkey-Gobbler.png"
+  },
+  {
+    "Manufacturer": "Rinehart",
+    "Type": "Turkey",
+    "Name": "Strutting Turkey",
+    "Image": "Turkey-StruttingTurkey.png"
+  },
+  {
+    "Manufacturer": "Rinehart",
+    "Type": "Turkey",
+    "Name": "Tom Turkey",
+    "Image": "Turkey-TomTurkey.png"
+  },
+  {
+    "Manufacturer": "Rinehart",
+    "Type": "Varmint",
+    "Name": "Bobcat",
+    "Image": "Varmint-Bobcat.png"
+  },
+  {
+    "Manufacturer": "Rinehart",
+    "Type": "Varmint",
+    "Name": "Raccoon",
+    "Image": "Varmint-Raccoon.png"
+  },
+  {
+    "Manufacturer": "Rinehart",
+    "Type": "Varmint",
+    "Name": "Skunk",
+    "Image": "Varmint-Skunk.png"
+  }
+];
+
+/* Groups of Targets */
+rinehart_targets = [];
+
+targets.forEach(function(t) {
+	target = {
+		url: 'targets/' + t.Manufacturer + '/' + t.Image,
+		caption: t.Name
+	};
+	rinehart_targets.push(target);
+});
+
+
+var app = new Framework7({
+  // App root element
+  root: '#app',
+  // App Name
+  name: 'My App',
+  // App id
+  id: 'com.myapp.test',
+  // Enable swipe panel
+  panel: {
+    swipe: 'left',
+  },
+  // Add default routes
+  routes: [
+    {
+      path: '/about/',
+      url: 'about.html',
+    },
+  ],
+  // ... other parameters
+});
+
+var mainView = app.views.create('.view-main');
+
+var $$ = Dom7;
+
+
+/*=== Default standalone ===*/
+var myPhotoBrowserStandalone = app.photoBrowser.create({
+    photos: rinehart_targets
+});
+//Open photo browser on click
+$$('.pb-standalone').on('click', function () {
+    myPhotoBrowserStandalone.open();
+});
+
+/*=== Popup ===*/
+var myPhotoBrowserPopup = app.photoBrowser.create({
+    photos: rinehart_targets,
+    type: 'popup'
+});
+$$('.pb-popup').on('click', function () {
+    myPhotoBrowserPopup.open();
+});
+
+/*=== As Page ===*/
+var myPhotoBrowserPage = app.photoBrowser.create({
+    photos: rinehart_targets,
+    type: 'page',
+    backLinkText: 'Back'
+});
+$$('.pb-page').on('click', function () {
+    myPhotoBrowserPage.open();
+});
+
+
